b5f2b8732e0d19615424262714744f978c0dc8e5
Simon Brodersen-Almind <sba@psibi.dk>
2025-03-27 13:03:45 +0100

    Add static requirements.txt file for simplicity

--- function_app.py
+++ function_app.py
@@ -2,4 +2,7 @@ import azure.functions as func
 
 from api import app as fastapi_app
 
-app = func.AsgiFunctionApp(app=fastapi_app, http_auth_level=func.AuthLevel.ANONYMOUS)
+app = func.AsgiFunctionApp(
+    app=fastapi_app,
+    http_auth_level=func.AuthLevel.ANONYMOUS
+)
